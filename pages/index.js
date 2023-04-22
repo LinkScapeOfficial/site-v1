c4b06aea2150adb3d2d05ca0e7c5bb58c135c1bd
Thomas Wu <62056970+thomaswcy@users.noreply.github.com>
2023-04-22 10:51:48 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -29,17 +29,6 @@ import Team from './team'
 
 const DocsPage = () => (
   <>
-    <Meta
-      as={Head}
-      name="LinkScape"
-      title="LinkScape"
-    description="A non-profit organization which works for the future of open-source software"
-    color="#ec3750"
-    manifest="/site.webmanifest"
-    />
-    <head>
-      <script async defer data-website-id="6d581cea-17fa-4aa6-9c62-742bbb3f6198" src="https://analytics.linkscape.app/umami.js"></script>
-    </head>
     <Box as="header" sx={{ bg: '#000000' }}>
 	  <br />
 	  <br />
